dfa9b990c8d0fce62ab92e433d392e800f846e07
Tim Gretler <tim.gretler@dfinity.org>
2022-08-13 21:31:04 +0000

    .

---
+++ linux/home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+let
+  common-programs = import ../common/home-manager.nix { pkgs = pkgs; }; 
+in
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "tim";
+  home.homeDirectory = "/home/tim";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.05";
+    
+  nixpkgs.config.allowUnfree = true;    
+  home.enableNixpkgsReleaseCheck = false;
+  home.packages = pkgs.callPackage ./packages.nix {};
+  programs = common-programs;
+
+}
