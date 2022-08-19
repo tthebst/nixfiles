48346551cdf70c6b67cdfa1c1d76ec3ff30943f5
tim gretler <gretler.tim@gmail.com>
2022-08-19 09:08:03 +0200

    .

---
+++ flakes/lighthouse/flake.nix
@@ -0,0 +1,28 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+
+        defaultApp = utils.lib.mkApp {
+          drv = self.defaultPackage."${system}";
+        };
+
+        devShell = with pkgs; mkShell {
+          buildInputs = [ clang libclang cmake rustfmt pre-commit protobuf rustup rustPackages.clippy pkg-config openssl go-ethereum];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          LIBCLANG_PATH = "${pkgs.llvmPackages_11.libclang.lib}/lib";
+          PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";  
+        };
+      });
+}
