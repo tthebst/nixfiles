dea3f55903489b6b4f299b9dd1b0fba607e95eb5
tim gretler <gretler.tim@gmail.com>
2022-10-16 11:13:58 +0200

    .

--- flakes/dfx/flake.nix
+++ flakes/dfx/flake.nix
@@ -19,12 +19,21 @@
           url = "https://github.com/dfinity/sdk/releases/download/${version}/${name}-x86_64-linux.tar.gz";
           sha256 = "d0d85c09e83681c08e5babbb6fac04348af571165fc0ef7dfea775c5b9fa8720";
         };
-
+        nativeBuildInputs = [
+          autoPatchelfHook
+        ];
         sourceRoot = ".";
 
         installPhase = ''
           install -m755 -D dfx $out/bin/dfx
+          runHook postInstall
         '';
+        postIntstall = ''
+          $out/bin/dfx cache install 
+          dfxcache = $out/bin/dfx cache show;
+          autoPatchelf "$dfxcache/*"
+        '';
+        
 
       };
   };
